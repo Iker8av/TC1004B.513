ee9f4b69384aef77f905c12c8f50de78b30c1e2f
Iker Ochoa <iker8avv@gmail.com>
2022-10-11 13:24:25 +0000

    Segunda Practica - Senales

---
+++ signal.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+int con
+void holaMundo(int signalNumber){
+    if (signalNumber == 10){
+        printf("Es la senal 10\n");
+    } else {
+        printf("Es otra senal\n");
+    }
+    con = 2;
+}
+
+void nomematas(int sig){
+    printf("JAJAJA, no me matas\n");
+}
+
+int main(){
+    signal(12, holaMundo);
+    signal(10, holaMundo);
+    signal(2, nomematas);
+
+    con = 1;
+
+    while (con == 1){
+        printf("Estoy trabajando\n");
+        sleep(1);
+    }
+
+    printf("Nunca llega\n");
+
+    return 0;
+}
